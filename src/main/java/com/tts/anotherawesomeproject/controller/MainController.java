f063765fefd5bec48d55beb517039f64a21b7e19
Mary Sutherland <mary.sutherland.atx@gmail.com>
2021-02-25 16:44:38 -0600

    first commit

---
+++ src/main/java/com/tts/anotherawesomeproject/controller/MainController.java
@@ -0,0 +1,23 @@
+package com.tts.anotherawesomeproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "index";
+	}
+	
+	@RequestMapping("/tts")
+	public String tts() {
+		return "index";
+	}
+	
+	@RequestMapping("/glob")
+	public String glob() {
+		return "index2";
+	}
+}
